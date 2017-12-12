3811705d997f0e7a13d1c2ab2b76311c07aa5950
Eric Cline <ecline@leapfrogonline.com>
2017-12-12 12:26:28 -0600

    finish day 12

--- aoc/days/day12.py
+++ aoc/days/day12.py
@@ -1,7 +1,6 @@
 from aoc.shared import open_file
 
 
-
 def parse_input(raw_pipes):
     pipes = {}
     for raw_pipe in raw_pipes:
@@ -10,24 +9,30 @@ def parse_input(raw_pipes):
     return pipes
 
 
+def group(program, pipes):
+    connected = set([program])
+    checked = set()
 
-
-
-def can_talk_to(program, pipes):
-    connected = set()
-
-    to_check = pipes[program]
-    while to_check:
-        next_check = []
-        for p in to_check:
-            if p not in connected:
-                next_check.append(p)
-            connected.add(p)
-        to_check = next_check
-
+    while connected.difference(checked):
+        for p in connected.difference(checked):
+            for k, v in pipes.items():
+                if p == k or p in v:
+                    connected.update(set(v))
+                    connected.add(k)
+            checked.add(p)
     return connected
+
+
+def groups(pipes):
+    groups = []
+    for p in pipes:
+        g = group(p, pipes)
+        if g not in groups:
+            groups.append(g)
+    return groups
 
 
 if __name__ == '__main__':
     pipes = parse_input(open_file("day12").read().splitlines())
-    print(can_talk_to('0', pipes))
+    print(len(group('0', pipes)))
+    print(len(groups(pipes)))
--- tests/test_day12.py
+++ tests/test_day12.py
@@ -1,11 +1,11 @@
 import unittest
 
-from aoc.days.day12 import can_talk_to, parse_input
+from aoc.days.day12 import group, groups
 
 
 class TestDay12(unittest.TestCase):
 
-    def test_can_talk_to_returns_right_stuff(self):
+    def test_group(self):
         pipes = {
             '0': ['2'],
             '1': ['1'],
@@ -15,5 +15,16 @@ class TestDay12(unittest.TestCase):
             '5': ['6'],
             '6': ['4', '5']
         }
-        self.assertEqual(set(['0', '2', '3', '4', '5', '6']), can_talk_to('0', pipes))
+        self.assertEqual(set(['0', '2', '3', '4', '5', '6']), group('0', pipes))
 
+    def test_group(self):
+        pipes = {
+            '0': ['2'],
+            '1': ['1'],
+            '2': ['0', '3', '4'],
+            '3': ['2', '4'],
+            '4': ['2', '3', '6'],
+            '5': ['6'],
+            '6': ['4', '5']
+        }
+        self.assertEqual([set(['0', '2', '3', '4', '5', '6']), set(['1'])], groups(pipes))
